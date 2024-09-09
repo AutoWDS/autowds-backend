1b3ae485202e79a029ae1cfa3fec1c7ec269c95e
holmofy <hff1996723@163.com>
2024-09-09 19:41:03 +0800

    task

--- src/dto/task.rs
+++ src/dto/task.rs
@@ -1,8 +1,17 @@
 use serde::Deserialize;
+use serde_json::Value;
 use validator::Validate;
 
 #[derive(Debug, Deserialize, Validate)]
 pub struct ScraperTaskQuery {
-    #[validate(length(max = 60, message = "查询名称过长"))]
+    #[validate(length(max = 80, message = "查询名称过长"))]
     pub name: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Validate)]
+pub struct ScraperTaskReq {
+    #[validate(length(max = 80, message = "查询名称过长"))]
+    pub name: String,
+
+    pub rule: Value,
 }
--- src/http/task.rs
+++ src/http/task.rs
@@ -1,13 +1,14 @@
-use crate::dto::task::ScraperTaskQuery;
+use crate::dto::task::{ScraperTaskQuery, ScraperTaskReq};
 use crate::model::prelude::ScraperTask;
 use crate::model::scraper_task;
 use crate::utils::jwt::Claims;
 use anyhow::Context;
-use sea_orm::{ColumnTrait, DbConn, EntityTrait, QueryFilter};
+use itertools::Itertools;
+use sea_orm::{ActiveModelTrait, ColumnTrait, DbConn, EntityTrait, QueryFilter, Set};
 use spring_sea_orm::pagination::{Pagination, PaginationExt};
 use spring_web::axum::Json;
-use spring_web::error::Result;
-use spring_web::extractor::{Component, Query};
+use spring_web::error::{KnownWebError, Result};
+use spring_web::extractor::{Component, Path, Query};
 use spring_web::{axum::response::IntoResponse, get};
 use spring_web::{delete, patch, post, put};
 
@@ -32,23 +33,93 @@ async fn query_task(
 }
 
 #[post("/task")]
-async fn add_task() -> Result<impl IntoResponse> {
-    Ok("")
+async fn add_task(
+    claims: Claims,
+    Component(db): Component<DbConn>,
+    Json(body): Json<ScraperTaskReq>,
+) -> Result<impl IntoResponse> {
+    let task = scraper_task::ActiveModel {
+        user_id: Set(claims.uid),
+        name: Set(body.name),
+        rule: Set(body.rule),
+        ..Default::default()
+    }
+    .insert(&db)
+    .await
+    .context("save scraper task failed")?;
+    Ok(Json(task))
 }
 
 #[post("/task/batch")]
-async fn add_batch_task() -> Result<impl IntoResponse> {
-    Ok("")
+async fn add_batch_task(
+    claims: Claims,
+    Component(db): Component<DbConn>,
+    Json(batch): Json<Vec<ScraperTaskReq>>,
+) -> Result<impl IntoResponse> {
+    if batch.len() > 10 {
+        Err(KnownWebError::bad_request("任务过多无法保存"))?;
+    }
+    let batch = batch
+        .into_iter()
+        .map(|m| scraper_task::ActiveModel {
+            user_id: Set(claims.uid),
+            name: Set(m.name),
+            rule: Set(m.rule),
+            ..Default::default()
+        })
+        .collect_vec();
+    let r = ScraperTask::insert_many(batch)
+        .exec(&db)
+        .await
+        .context("batch save scraper task failed")?;
+    Ok(Json(r.last_insert_id))
 }
 
 #[get("/task/:id")]
-async fn get_task() -> Result<impl IntoResponse> {
-    Ok("")
+async fn get_task(
+    claims: Claims,
+    Path(id): Path<i64>,
+    Component(db): Component<DbConn>,
+) -> Result<impl IntoResponse> {
+    let task = ScraperTask::find_by_id(id)
+        .one(&db)
+        .await
+        .context("find scraper task failed")?
+        .ok_or_else(|| KnownWebError::not_found("任务不存在"))?;
+
+    if task.user_id != claims.uid {
+        Err(KnownWebError::forbidden("数据无权访问"))?;
+    }
+
+    Ok(Json(task))
 }
 
 #[delete("/task/:id")]
-async fn delete_task() -> Result<impl IntoResponse> {
-    Ok("")
+async fn delete_task(
+    claims: Claims,
+    Path(id): Path<i64>,
+    Component(db): Component<DbConn>,
+) -> Result<impl IntoResponse> {
+    let task = ScraperTask::find_by_id(id)
+        .one(&db)
+        .await
+        .context("find scraper task failed")?
+        .ok_or_else(|| KnownWebError::not_found("任务不存在"))?;
+
+    if task.user_id != claims.uid {
+        Err(KnownWebError::forbidden("数据无权访问"))?;
+    }
+
+    scraper_task::ActiveModel {
+        id: Set(task.id),
+        deleted: Set(true),
+        ..Default::default()
+    }
+    .save(&db)
+    .await
+    .context("save scraper task failed")?;
+
+    Ok(Json(task.id))
 }
 
 #[put("/task/:id")]
